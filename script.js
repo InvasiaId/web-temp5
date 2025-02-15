823e99387a7862cb79127e5fe64f505e04c3e61f
InvasiaId <invasiaid@gmail.com>
2025-02-15 14:53:44 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function startAnimation() {
+  const animation = document.querySelector('.animation');
+  const text = document.getElementById('text');
+  animation.style.animationPlayState = 'running'; 
+    
+}
